81ac96b2c47659205eacb03e89d10c6ea65d8b54
AlexandraS54 <93537145+AlexandraS54@users.noreply.github.com>
2023-10-09 23:10:35 +0200

    base jeu rpg

---
+++ rpg_essai/Player/player.gd
@@ -0,0 +1,86 @@
+extends CharacterBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const ROLL_SPEED = 120
+const FRICTION = 600
+
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+var roll_vector = Vector2.LEFT
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder 
+
+func _ready():
+	animationTree.active = true
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ROLL:
+			roll_state(delta)
+		
+		ATTACK:
+			attack_state(delta)
+		
+
+
+func move_state(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size()>0:
+			actionables[0].action()
+			return
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		animationTree.set("parameters/idle/blend_position", input_vector)
+		animationTree.set("parameters/run/blend_position", input_vector)
+		animationTree.set("parameters/attack/blend_position", input_vector)
+		animationTree.set("parameters/roll/blend_position", input_vector)
+		animationState.travel("run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION  * delta)
+	
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("roll")
+	move()
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("attack")
+
+func move():
+	move_and_slide()
+
+func roll_animation_finished():
+	velocity = Vector2.ZERO
+	state = MOVE
+
+func attack_animation_finished():
+	state = MOVE
